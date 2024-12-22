386a0f7e0f341995339c14435cb9a604780ad029
Chotu Kumar <ck4001252@gmail.com>
2024-12-22 16:39:02 +0530

    undefined data removed

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,8 +9,13 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
+
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
 
-export default eslintConfig;
+export default eslintConfig({
+  rules: {
+    "@typescript-eslint/no-explicit-any": "error"
+  }
+});
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,7 @@
 "use client";
 import React, { useState } from "react";
-import { HoveredLink, Menu, MenuItem, ProductItem } from "./ui/navbar-menu";
+import { HoveredLink, Menu, MenuItem,} from "./ui/navbar-menu";
 import { cn } from "@/lib/utils";
-import Link from "next/link";
 
 
 
@@ -13,26 +12,26 @@ export default function Navbar({ className }: { className?: string }) {
       className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}
     >
       <Menu  setActive={setActive}>
-        <Link href={'/'}>
+        <HoveredLink href={'/'}>
         <MenuItem setActive={setActive} active={active} item="Home">
         </MenuItem>
-        </Link>
-        <Link href={'/'}>
+        </HoveredLink>
+        <HoveredLink href={'/'}>
         <MenuItem setActive={setActive} active={active} item="Skills">
         
         </MenuItem>
-        </Link>
+        </HoveredLink>
 
-        <Link href={'/'}>
+        <HoveredLink href={'/'}>
         <MenuItem setActive={setActive} active={active} item="Projects">
           
         </MenuItem>
-        </Link>
-        <Link href={'/'}>
+        </HoveredLink>
+        <HoveredLink href={'/'}>
         <MenuItem setActive={setActive} active={active} item="Contact">
          
         </MenuItem>
-        </Link>
+        </HoveredLink>
       </Menu>
     </div>
   );
