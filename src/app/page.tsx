398c49903f7efc8615b1baa63856fb627ed52b1d
Chotu Kumar <ck4001252@gmail.com>
2025-08-27 17:51:20 +0530

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,7 @@ import { AboutMe } from "@/components/AboutMe";
 import Achievements from "@/components/Achievement"; // plural, keep your import style
 import ClientPageWrapper from "@/components/ClientPageWrapper";
 import { Contact } from "@/components/Contact";
-import GetInTouch from "@/components/GetInTouch";
 import { HeroSection } from "@/components/HeroSection";
-import { MyJourny } from "@/components/MyJourny";
 import { ProjectsWork } from "@/components/ProjectsWork";
 import { Skills } from "@/components/Skills";
 import { useRef } from "react";
